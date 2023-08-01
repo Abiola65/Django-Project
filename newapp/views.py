892f663eb6b101ad9d22988fe9df9cc02725018c
Abiola65 <abiolafolarin11@gmail.com>
2023-08-01 02:18:52 +0100

    outputing a variable

--- newapp/views.py
+++ newapp/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def homepage(request):
-    return render(request, 'index.html')
+    name = 'Antonio'
+    age = 15
+    return render(request, 'index.html', {'name':name, 'age':age})

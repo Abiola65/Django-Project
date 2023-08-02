a124df3509ec7dac61d5f0182fd04c9abe3b97f6
Abiola65 <abiolafolarin11@gmail.com>
2023-08-02 01:45:51 +0100

    outputing a variable

--- newapp/views.py
+++ newapp/views.py
@@ -2,6 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def homepage(request):
+    """
+    Function responds to website root HTTP request
+
+    Returns index.html template - no db data returned
+    
+    """
     name = 'Antonio'
     age = 15
-    return render(request, 'index.html', {'name':name, 'age':age})
+
+    print(name)
+    return render(request, 'index.html', {'name':name, 'age':age,})

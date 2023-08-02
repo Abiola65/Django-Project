78c40401864a8368677ec3dc226cae1313239961
Abiola65 <abiolafolarin11@gmail.com>
2023-08-02 02:48:21 +0100

    updating changes in my models

--- newapp/models.py
+++ newapp/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 
 class Customer(models.Model):
-    name = models.CharField(max_length=25)
+    name = models.CharField(max_length=10)
     age = models.IntegerField()
     mobile = models.IntegerField()
 
+    def __str__(self):
+        return self.name
+    

ae0841c62bdaad7a77e1be835951abd4fef121b2
Abiola65 <abiolafolarin11@gmail.com>
2023-08-06 16:52:53 +0100

    writting a django based view, and complting the project

--- newapp/urls.py
+++ newapp/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage),
-    path('addnew/', views.addnew)
+    path('addnew/', views.addnew),
+    path('test/', views.test),
 ]
--- newapp/views.py
+++ newapp/views.py
@@ -1,14 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import Customer
 from .forms import CustomerForm
 
+def test(request):
+    html = '<html><body>hello world</body></html>'
+    return HttpResponse(html)
+def calculation(a,b):
+    x = a+b
+    return x
+
+
 def addnew(request):
     if request.method == 'POST':
-        pass
+        formset=CustomerForm(request.POST)
+
+        if formset.is_valid():
+            formset.save()
+            return HttpResponseRedirect("/")
     else:
         formset = CustomerForm()
-        return render(request, 'addnew.html',{'formset':formset})
+        x = calculation(10,5)
+        return render(request, 'addnew.html',{'formset':formset, 'calc':x})
 
 def homepage(request):
     """
